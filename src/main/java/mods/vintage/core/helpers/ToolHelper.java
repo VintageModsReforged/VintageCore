e7fd5904ac64f98a01c6e66b3eda62322129fedf
CrossVas <cross.wanted.1.4@gmail.com>
2025-03-18 09:08:14 +0900

    harvest block method now takes metadata as a parameter to avoid metadata loss

--- src/main/java/mods/vintage/core/helpers/ToolHelper.java
+++ src/main/java/mods/vintage/core/helpers/ToolHelper.java
@@ -16,26 +16,25 @@ import java.util.List;
 
 public class ToolHelper {
 
-    public static boolean harvestBlock(World world, int x, int y, int z, EntityPlayer player) {
-        return harvestAndDrop(world, x, y, z, player, simpleHarvest());
+    public static boolean harvestBlock(World world, int x, int y, int z, int metadata, EntityPlayer player) {
+        return harvestAndDrop(world, x, y, z, metadata, player, simpleHarvest());
     }
 
     private static IDropCallback simpleHarvest() {
         return new IDropCallback() {
             @Override
-            public void handleServer(World world, Block block, int x, int y, int z, EntityPlayer player) {
-                int blockMetadata = world.getBlockMetadata(x, y, z);
-                block.harvestBlock(world, player, x, y, z, blockMetadata);
+            public void handleServer(World world, Block block, int x, int y, int z, int metadata, EntityPlayer player) {
+                block.harvestBlock(world, player, x, y, z, metadata);
             }
 
             @Override
-            public void handleClient(World world, Block block, int x, int y, int z, EntityPlayer player) {
+            public void handleClient(World world, Block block, int x, int y, int z, int metadata, EntityPlayer player) {
                 // NO-OP
             }
         };
     }
 
-    public static boolean harvestAndDrop(World world, int x, int y, int z, EntityPlayer player, IDropCallback callback) {
+    public static boolean harvestAndDrop(World world, int x, int y, int z, int metadata, EntityPlayer player, IDropCallback callback) {
         if (world.isAirBlock(x, y, z)) {
             return false;
         } else {
@@ -44,19 +43,18 @@ public class ToolHelper {
                 playerMP = (EntityPlayerMP)player;
             }
             Block block = Block.blocksList[world.getBlockId(x, y, z)];
-            int blockMetadata = world.getBlockMetadata(x, y, z);
-            if (!ForgeHooks.canHarvestBlock(block, player, blockMetadata)) {
+            if (!ForgeHooks.canHarvestBlock(block, player, metadata)) {
                 return false;
             } else {
                 if (player.capabilities.isCreativeMode) {
                     if (!world.isRemote) {
-                        block.onBlockHarvested(world, x, y, z, blockMetadata, player);
+                        block.onBlockHarvested(world, x, y, z, metadata, player);
                     } else {
-                        world.playAuxSFX(2001, x, y, z, world.getBlockId(x, y, z) | blockMetadata << 12);
+                        world.playAuxSFX(2001, x, y, z, world.getBlockId(x, y, z) | metadata << 12);
                     }
 
                     if (block.removeBlockByPlayer(world, player, x, y, z)) {
-                        block.onBlockDestroyedByPlayer(world, x, y, z, blockMetadata);
+                        block.onBlockDestroyedByPlayer(world, x, y, z, metadata);
                     }
 
                     if (!world.isRemote) {
@@ -67,21 +65,21 @@ public class ToolHelper {
                         Minecraft.getMinecraft().getNetHandler().addToSendQueue(new Packet14BlockDig());
                     }
                 } else {
-                    world.playAuxSFXAtEntity(player, 2001, x, y, z, world.getBlockId(x, y, z) | blockMetadata << 12);
+                    world.playAuxSFXAtEntity(player, 2001, x, y, z, world.getBlockId(x, y, z) | metadata << 12);
                     if (!world.isRemote) {
-                        block.onBlockHarvested(world, x, y, z, blockMetadata, player);
+                        block.onBlockHarvested(world, x, y, z, metadata, player);
                         if (block.removeBlockByPlayer(world, player, x, y, z)) {
-                            block.onBlockDestroyedByPlayer(world, x, y, z, blockMetadata);
-                            callback.handleServer(world, block, x, y, z, player);
+                            block.onBlockDestroyedByPlayer(world, x, y, z, metadata);
+                            callback.handleServer(world, block, x, y, z, metadata, player);
                         }
                         assert playerMP != null;
                         playerMP.playerNetServerHandler.sendPacketToPlayer(new Packet53BlockChange());
                     } else {
                         if (block.removeBlockByPlayer(world, player, x, y, z)) {
-                            block.onBlockDestroyedByPlayer(world, x, y, z, blockMetadata);
+                            block.onBlockDestroyedByPlayer(world, x, y, z, metadata);
                         }
                         Minecraft.getMinecraft().getNetHandler().addToSendQueue(new Packet14BlockDig());
-                        callback.handleClient(world, block, x, y, z, player);
+                        callback.handleClient(world, block, x, y, z, metadata, player);
                     }
                 }
 
@@ -91,8 +89,8 @@ public class ToolHelper {
     }
 
     public interface IDropCallback {
-        void handleServer(World world, Block block, int x, int y, int z, EntityPlayer player);
-        void handleClient(World world, Block block, int x, int y, int z, EntityPlayer player);
+        void handleServer(World world, Block block, int x, int y, int z, int metadata, EntityPlayer player);
+        void handleClient(World world, Block block, int x, int y, int z, int metadata, EntityPlayer player);
     }
 
     public static List<BlockPos> getAOE(EntityPlayer player, BlockPos pos, int radius) {
