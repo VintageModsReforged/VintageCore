99a63df5fa6b196654f123f8a620ad2b2cc21b4b
CrossVas <cross.wanted.1.4@gmail.com>
2025-03-26 15:55:56 +0900

    feat: easy way to change gamemodes between survival and creative

---
+++ src/main/java/mods/vintage/core/platform/GameModeSwitcher.java
@@ -0,0 +1,35 @@
+package mods.vintage.core.platform;
+
+import mods.vintage.core.platform.lang.FormattedTranslator;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
+import net.minecraft.world.EnumGameType;
+import org.lwjgl.input.Keyboard;
+
+public class GameModeSwitcher {
+
+    private static boolean isF4Pressed = false;
+    private static int modeIndex = 0;
+    private static final EnumGameType[] MODES = {
+            EnumGameType.SURVIVAL,
+            EnumGameType.CREATIVE
+    };
+
+    public static void onKeyInput() {
+        Minecraft mc = Minecraft.getMinecraft();
+        EntityClientPlayerMP player = mc.thePlayer;
+
+        if (player == null) return;
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_F4)) {
+            if (!isF4Pressed) {
+                modeIndex = (modeIndex + 1) % MODES.length;
+            }
+            isF4Pressed = true;
+        } else if (isF4Pressed) {
+            mc.playerController.setGameType(MODES[modeIndex]);
+            player.addChatMessage(FormattedTranslator.GOLD.format("chat.gamemode.switched", FormattedTranslator.AQUA.literal(MODES[modeIndex].getName())));
+            isF4Pressed = false;
+        }
+    }
+}
