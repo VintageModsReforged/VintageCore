6b450dc46f2ad84e825e13e06510b51f4267a145
CrossVas <cross.wanted.1.4@gmail.com>
2025-03-29 01:09:22 +0900

    test: testing new id config

--- src/main/java/mods/vintage/core/platform/config/VintageConfig.java
+++ src/main/java/mods/vintage/core/platform/config/VintageConfig.java
@@ -6,11 +6,13 @@ import java.io.File;
 
 public class VintageConfig extends Configuration {
 
-    public static AutoID testItemID = new AutoID(mods.vintage.core.platform.config.AutoID.Mode.ITEM, "testItem", 400);
-    public static AutoID testBlockID = new AutoID(mods.vintage.core.platform.config.AutoID.Mode.BLOCK, "testBlock", 800);
+    public static AutoID testItemID = new AutoID(AutoID.Mode.ITEM, "testItem", 4000);
+    public static AutoID testBlockID = new AutoID(AutoID.Mode.BLOCK, "testBlock", 800);
 
     public VintageConfig(File file) {
         super(file);
         load();
+        this.addCustomCategoryComment("item", "Test Comment for item id section");
+        this.addCustomCategoryComment("block", "Test Comment for block id section");
     }
 }
