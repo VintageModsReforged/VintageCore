deaf324fafedce12f897d04c7705cbdaafc25f5d
CrossVas <cross.wanted.1.4@gmail.com>
2025-03-29 22:21:56 +0900

    update: version index

--- src/main/java/mods/vintage/core/Refs.java
+++ src/main/java/mods/vintage/core/Refs.java
@@ -5,6 +5,6 @@ public class Refs {
     public static final String ID = "VintageCore";
     public static final String NAME = "Vintage Core";
     public static final String LOG_NAME = "Vintage Suite";
-    public static final String VERSION = "1.5.2-1.0.5.1";
+    public static final String VERSION = "1.5.2-1.0.6";
     public static final String MC_VERSION = "[1.5.2]";
 }
