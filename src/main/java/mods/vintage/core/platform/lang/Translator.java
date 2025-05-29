fcbf0fde3f3d5e712fa118fc7f3a5b30422e04c5
CrossVas <cross.wanted.1.4@gmail.com>
2025-05-29 11:25:04 +0900

    change: use gray color as default

--- src/main/java/mods/vintage/core/platform/lang/Translator.java
+++ src/main/java/mods/vintage/core/platform/lang/Translator.java
@@ -1,11 +1,9 @@
 package mods.vintage.core.platform.lang;
 
-import net.minecraft.util.StatCollector;
-
 public class Translator {
 
     public static String format(String key, Object... args) {
-        return StatCollector.translateToLocalFormatted(key, args);
+        return FormattedTranslator.GRAY.format(key, args);
     }
 
     public static String format(boolean b) {
